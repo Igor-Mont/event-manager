4185141316c75dc6b4cf0b7463ac308adf93bada
Igor-Mont <igornathandev@gmail.com>
2024-04-02 15:43:32 -0300

    feat: add route to get an attendee badge

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import {
 import { createEevent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
+import { getAttendeeBagde } from "./routes/get-attendee-badge";
 
 const app = fastify();
 
@@ -15,6 +16,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createEevent);
 app.register(registerForEvent);
 app.register(getEvent);
+app.register(getAttendeeBagde);
 
 app.listen({ port: 3030 }).then(() => {
   console.log("Server is running on port 3030 🔥.");
