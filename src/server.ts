94d90fc46a36d75989d2b7a28367299ed204b038
Igor-Mont <igornathandev@gmail.com>
2024-04-02 15:24:33 -0300

    feat: add route to get event

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import {
 } from "fastify-type-provider-zod";
 import { createEevent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
 
 const app = fastify();
 
@@ -13,6 +14,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEevent);
 app.register(registerForEvent);
+app.register(getEvent);
 
 app.listen({ port: 3030 }).then(() => {
   console.log("Server is running on port 3030 🔥.");
