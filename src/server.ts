bf89951034f83d0859cbe29bbf69ed3d5ddb4d5e
Igor-Mont <igornathandev@gmail.com>
2024-04-03 12:38:23 -0300

    feat: register cors

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import {
   validatorCompiler,
   jsonSchemaTransform,
 } from "fastify-type-provider-zod";
+import fastifyCors from "@fastify/cors";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
 import { createEevent } from "./routes/create-event";
@@ -15,6 +16,10 @@ import { getEventAttendees } from "./routes/get-event-attendees";
 import { errorHandler } from "./error-handler";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 app.register(fastifySwagger, {
   swagger: {
@@ -46,6 +51,6 @@ app.register(getEventAttendees);
 
 app.setErrorHandler(errorHandler);
 
-app.listen({ port: 3030 }).then(() => {
+app.listen({ port: 3030, host: "0.0.0.0" }).then(() => {
   console.log("Server is running on port 3030 🔥.");
 });
