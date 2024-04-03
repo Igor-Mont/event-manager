2e5c7e6cfd7bb17f0896ba2a704a10693836dbbd
Igor-Mont <igornathandev@gmail.com>
2024-04-03 09:11:09 -0300

    feat: add route to get event attendees

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBagde } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
+import { getEventAttendees } from "./routes/get-event-attendees";
 
 const app = fastify();
 
@@ -19,6 +20,7 @@ app.register(registerForEvent);
 app.register(getEvent);
 app.register(getAttendeeBagde);
 app.register(checkIn);
+app.register(getEventAttendees);
 
 app.listen({ port: 3030 }).then(() => {
   console.log("Server is running on port 3030 🔥.");
